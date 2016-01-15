e62509c510a8fafde090d20f7e10cef23fbe03cd
Vitaly Dryukov <miniwe@mail.ru>
2016-01-16 00:29:36 +0100

    fix for meteor 1.2 Files to Assets

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
 
-  api.addFiles([
+  api.addAssets([
     'jquery.range2dslider.css',
     'jquery.range2dslider.js'
   ], 'client');
